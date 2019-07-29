9b62ca8391ae778ed8c736f77ba5619245375451
Rocktim <rajkumar.rocktim@gmail.com>
2019-07-30 00:49:03 +0530

    src > App.js : players,balance and value display done

--- src/App.js
+++ src/App.js
@@ -6,19 +6,45 @@ import lottery from "./lottery";
 
 class App extends Component {
   state = {
-    manager: ""
+    manager: "",
+    players: [],
+    balance: "",
+    value: ""
   };
 
   async componentDidMount() {
     const manager = await lottery.methods.manager().call();
-    this.setState({ manager });
+    const players = await lottery.methods.getPlayers().call();
+    const balance = await lottery.methods.getTotalBalance().call();
+    this.setState({ manager, players, balance });
   }
 
   render() {
     return (
       <div>
-        <h2>Lottery Contract</h2>
+        <h2>Lottery Contract!</h2>
         <p>This contract is managed by {this.state.manager}</p>
+        <p>
+          There are currently {this.state.players.length} people entered,
+          competing to win {web3.utils.fromWei(this.state.balance, "ether")}{" "}
+          ether!
+        </p>
+
+        <hr />
+
+        <form>
+          <h4>Want to try your luck?</h4>
+          <div>
+            <label>Amount of ether to enter </label>
+            <input
+              value={this.state.value}
+              onChange={event => this.setState({ value: event.target.value })}
+            />
+          </div>
+
+          <button>Enter</button>
+
+        </form>
       </div>
     );
   }
