89a4fbc15026cf5db90c2aa2df060a5b4fe4636d
Rocktim <rajkumar.rocktim@gmail.com>
2019-07-30 00:09:05 +0530

    src > App.js : display info from contract to react application

--- src/lottery.js
+++ src/lottery.js
@@ -5,4 +5,4 @@ const address = "0x82107fD09650404bbC60960e3df98a8196f37944";
 const abi =
     '[{"constant":true,"inputs":[],"name":"getTotalBalance","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"manager","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"pickWinner","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getPlayers","outputs":[{"name":"","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"enter","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"players","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}]';
 
-export default new web3(abi, address);
+export default new web3.eth.Contract(JSON.parse(abi), address);
